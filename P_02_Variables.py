0212166ebe4f3b9e855658ad91b803efbfd8dd1a
TaylorAdam23 <adamt4171@masseyhigh.school.nz>
2025-05-08 10:44:18 +1200

    Variable Basics
    Ever so slightly easier code. STILL BE CAREFUL WHEN YOU USE THIS AS YOUR PC MAY EXPLODE !!!!!!!!!!!!!!!!

---
+++ P_02_Variables.py
@@ -0,0 +1,16 @@
+greeting = "Hi"
+my_name = "Mine"
+love_programming = True
+#favnum auta answers the question its given
+#print (fav_num * 2) = 14
+#Numstring give all the times its multiplyied by that number
+#print(num_string * 5) = 55555
+
+fav_num = 7
+num_string = "5"
+
+PI = 3.14
+
+a_01_my_num = 7
+
+
