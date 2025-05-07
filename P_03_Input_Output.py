7e31995fcb92624d94030ce0669149ffb68661ab
TaylorAdam23 <adamt4171@masseyhigh.school.nz>
2025-05-08 11:03:27 +1200

    Getting Input, Attracting Output
    Pretty simple code to be honest. But still a high change of your pc creating a black hole and destroying the entire universe. Use with caution.

---
+++ P_03_Input_Output.py
@@ -0,0 +1,20 @@
+#Ask of users name
+username = input("What is your name ? ")
+
+#ask for uesrs favourite number (integer)
+fav_num = int(input("What is your favourite number ? "))
+
+#double, halve and square the user's favourite number
+double  = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+#greet user
+print(f"\nHi {username}, your favourite number is {fav_num}")
+
+#output results of double, halving and squaring fav num
+print(f"double {fav_num} is {double}.")
+print(f"double {fav_num} is {halve}.")
+print(f"double {fav_num} is {square}.")
+print()
+print("Have a nie day.")
