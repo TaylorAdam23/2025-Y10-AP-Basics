74208fcb10cdc5fabeadcb5a2010499341b54aca
TaylorAdam23 <adamt4171@masseyhigh.school.nz>
2025-05-12 12:51:38 +1200

    Ask for width
    Basic baby code even a monkey could write this

---
+++ P_04_Get_Width.py
@@ -0,0 +1,16 @@
+#ask user for width and loop until they enter a number that is more than zero
+
+error = "Please enter a number that is more than zero\n"
+while True:
+
+    try:
+        # Ask the user for number
+        width = float(input("Width: "))
+        # Check that the number is more than zero
+        if width > 0:
+            break
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
